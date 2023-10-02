12a6ee7f5f1662520d24d46f41482f6622d72c71
jc <wnwlcks123@gmail.com>
2023-10-02 14:46:06 +0900

    HTML Webpack Plugin

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ package.json 파일 생성
 # webpack loader
 웹팩이 웹 애플리케이션을 해석할 때 자바스크립트 파일이 아닌 웹 자원(HTML, CSS, Images, 폰트 등) 들을 변환할 수 있도록 도와주는 속성.   
 `$ npm i -D css-loader style-loader sass sass-loader`
+
+# HTML Webpack Plugin
+HtmlWebPackPlugin 은 웹팩이 html 파일을 읽어서 html 파일을 빌드할 수 있게 해 준다.   
+`$ npm i -D html-webpack-plugin`   
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: 'development',
@@ -22,5 +23,11 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    })
+  ]
 }
